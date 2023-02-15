b875ebba0912786951ea8f3ccf53f4956c0c90ed
MindoroJonas <123728404+Reeve0@users.noreply.github.com>
2023-02-15 19:59:13 +0800

    Revert "Revert "commit form""
    This reverts commit 1c4faef67ba1ab384606b78787ba970683723fd5.

---
+++ bootstrap-5.3.0-alpha1-examples/index1.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<form action="index.php" method="post">
+First Name: <input type="text" name="fname"><br>
+Last Name: <input type="text" name="lname"><br>
+Age: <input type="text" name="age"><br>
+Email: <input type="text" name="email"><br>
+Details: <input type="text" name="detail"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
